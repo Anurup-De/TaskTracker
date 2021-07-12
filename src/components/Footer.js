2340ebfa800b71af2376f971c0cdadf8d769c114
Anurup De <anurupde2012@gmail.com>
2021-07-12 06:40:41 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+               Copyright &copy; 2021
+            </p>
+            <a href="https://www.linkedin.com/in/anurupde/">About</a>
+        </footer>
+    )
+}
+
+export default Footer
